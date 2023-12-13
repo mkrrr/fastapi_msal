da0ca0afff5d245e2f045528ff68a72b84a8b703
Markian Yskout <markian.yskout@atlascopco.com>
2023-12-13 21:29:07 +0100

    debug traces

--- fastapi_msal/clients/async_conf_client.py
+++ fastapi_msal/clients/async_conf_client.py
@@ -81,6 +81,7 @@ class AsyncConfClient:
         return AuthCode.parse_obj_debug(to_parse=auth_code)
 
     async def finalize_auth_flow(self, auth_code_flow: AuthCode, auth_response: AuthResponse) -> AuthToken:
+        print(f"XXXXXXX authcode {auth_code_flow}\n resp {auth_response}")
         auth_token: StrsDict = await self.__execute_async__(
             self._cca.acquire_token_by_auth_code_flow,
             auth_code_flow=auth_code_flow.model_dump(exclude_none=True),
